7a7008722062cac932c3f10e8e55be2b0671a2e5
cesar banchio <cbanchio5@gmail.com>
2022-08-15 20:48:18 +0200

    added send reset password mail functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,8 @@ const catchAsync = require('./../utils/catchAsync')
 const jwt = require('jsonwebtoken')
 const AppError = require('./../utils/appError')
 const util = require('util')
+const sendMail = require('./../utils/email')
+
 
 const signToken = id => {
    return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -109,17 +111,41 @@ exports.restricTo = (...roles) => {
 exports.forgotPassword = catchAsync(async (req, res, next) => {
 //1Get user based on posted email
 
-const user = await User.findOne({email: req.body.email})
+  const user = await User.findOne({email: req.body.email})
 
-if(!user) {
-  next(new AppError('No user with that email', 404))
-}
+  if(!user) {
+    next(new AppError('No user with that email', 404))
+  }
 
-//2generate random token
-const resetToken = user.createPasswordResetToken();
-await user.save({ validateBeforeSave: false });
+  //2generate random token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
 
-//3send as email
+  //3send as email
+
+  const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`
+  const message = `Forgot your password? Submit a PATCH request with your new password and password confirm to : ${resetURL}.\nIf you didn't forget your password, please ignore this email.`
+
+  try {
+    await sendMail({
+      email:user.email,
+      subject:'Your password reset token (valid for 10 min)',
+      message
+    });
+    res.status(200).json({
+      status:'success',
+      message:'Token sent to email'
+    })
+  }
+  catch(err) {
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save({ validateBeforeSave:false })
+  return next(new AppError('There was an error sending the email. Try again later', 500))
+
+  }
+
+
 })
 
 exports.resetPassword = (req, res, next) => {
