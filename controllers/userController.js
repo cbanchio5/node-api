6e3b6b1f29d86aa97a93ef1767224da44ff22c1c
cesar banchio <cbanchio5@gmail.com>
2022-08-17 12:40:30 +0200

    added update user info functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,14 @@ const AppError = require('../utils/appError');
 const User = require('./../models/userModel')
 const catchAsync = require('./../utils/catchAsync')
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {}
+  Object.keys(obj).forEach(el => {
+    if(allowedFields.includes(el)) newObj[el] = obj[el];
+  })
+  return newObj
+}
+
 exports.getAllUsers = catchAsync(async(req, res, next) => {
   const users = await User.find();
   res.status(200).json({
@@ -41,3 +49,23 @@ exports.deleteUser = (req, res) => {
     message:'This route is not yet defined'
   });
 }
+
+exports.updateMe = catchAsync(async(req, res, next) => {
+
+  //1.) Create error if user post password data
+  if(req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('This route is not for password updates. Please use /updateMyPassword', 400))
+  }
+  //2.) Update user document
+  const filteredBody = filterObj(req.body, 'name', 'email')
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new:true,
+    runValidators:true});
+
+  res.status(200).json({
+    status:'success',
+    data: {
+      user: updatedUser
+    }
+  });
+})
