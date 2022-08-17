93a4933fcde69dd1ed75882d4704bd3595823cb6
cesar banchio <cbanchio5@gmail.com>
2022-08-17 10:49:00 +0200

    resetpassword functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken')
 const AppError = require('./../utils/appError')
 const util = require('util')
 const sendMail = require('./../utils/email')
+const crypto = require('crypto')
 
 
 const signToken = id => {
@@ -148,6 +149,35 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
 })
 
-exports.resetPassword = (req, res, next) => {
+exports.resetPassword = catchAsync(async(req, res, next) => {
+  //1.) get user based on token
 
-}
+  const hashedToken = crypto.createHash('sha256').update(req.params.token).digest('hex')
+
+  const user = await User.findOne({ passwordResetToken: hashedToken, passwordResetExpires: {$gt: Date.now()} })
+
+  //2.) If token has not expired, and there is a usser, set the new password
+
+  if(!user) {
+    return next(new AppError('Token is invalid or has expired', 400))
+  }
+
+  //3.) Update changedPasswordAt property for the user
+
+  user.password = req.body.password
+  user.passwordConfirm = req.body.password
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+
+  await user.save()
+
+
+  //4.) Log the user in send JWT
+
+  const token = signToken(user._id)
+  res.status(200).json({
+    status:'success',
+    token
+  })
+
+})
