bf05fd31672c5ddce193a293fb97bd8267230ba9
cesar banchio <cbanchio5@gmail.com>
2022-08-17 12:21:26 +0200

    solved update password functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,6 +13,18 @@ const signToken = id => {
   });
 }
 
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id)
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user
+    }
+  })
+
+}
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name:req.body.name,
@@ -21,16 +33,9 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm:req.body.passwordConfirm,
     role: req.body.role
   });
+  createSendToken(newUser, 200, res)
 
-  const token = signToken(newUser._id)
 
-  res.status(201).json({
-    status: 'success',
-    token,
-    data: {
-      user: newUser
-    }
-  })
 })
 
 
@@ -55,11 +60,7 @@ exports.login = catchAsync(async (req, res, next) => {
 
 
   //3 If everything is ok, send token to client
-  const token = signToken(user._id)
-  res.status(200).json({
-    status:'success',
-    token
-  })
+  createSendToken(user, 200, res)
 
 })
 
@@ -174,10 +175,28 @@ exports.resetPassword = catchAsync(async(req, res, next) => {
 
   //4.) Log the user in send JWT
 
-  const token = signToken(user._id)
-  res.status(200).json({
-    status:'success',
-    token
-  })
+  createSendToken(user, 200, res)
 
+})
+
+exports.updatePassword = catchAsync(async(req, res, next) => {
+  //1.) Get user from collection
+
+  const user = await User.findById(req.user.id ).select('+password')
+
+
+  //2.) chech is posted password is correct
+
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  //3.) update password
+
+  user.password = req.body.password
+  user.passwordConfirm = req.body.passwordConfirm
+  await user.save()
+
+  //4.) log user in, send JWT
+  createSendToken(user, 200, res)
 })
