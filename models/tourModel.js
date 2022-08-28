fdbd73a4ea468b59ff1462aba4435c1dacd00926
cesar banchio <cbanchio5@gmail.com>
2022-08-28 20:09:32 +0200

    added distances routes

--- models/tourModel.js
+++ models/tourModel.js
@@ -191,10 +191,10 @@ const tourSchema = new mongoose.Schema({
 
   //aggregation middleware
 
-  tourSchema.pre('aggregate', function(next){
-    this.pipeline().unshift({$match: { secretTour:{$ne:true}}})
-    next();
-  });
+  // tourSchema.pre('aggregate', function(next){
+  //   this.pipeline().unshift({$match: { secretTour:{$ne:true}}})
+  //   next();
+  // });
 
   const Tour = mongoose.model('Tour', tourSchema);
 
