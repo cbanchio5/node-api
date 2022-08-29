035848521a0b173983f382e424f9b932a925747d
cesar banchio <cbanchio5@gmail.com>
2022-08-29 12:38:03 +0200

    added overview view with data

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 const reviewRouter = require('./routes/reviewRoutes')
+const viewRouter = require('./routes/viewRoutes')
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean')
 const hpp = require('hpp')
+
 
 
 
@@ -84,13 +86,8 @@ app.use((req, res, next) => {
 
 //Routes
 
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Cesar'
-  })
-})
 
+app.use('/', viewRouter)
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
